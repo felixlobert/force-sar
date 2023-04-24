6b51cc094525b54e2b56d16fec0f6a844246589f
Felix Lobert <57570374+felixlobert@users.noreply.github.com>
2023-04-24 10:56:11 +0200

    clean code before publishing to dockerhub (#9)
    * test format workflow before docker
    
    * Format code with black
    
    * bugfix branch name
    
    * test workflow
    
    * apply black to extensionless files
    
    ---------
    
    Co-authored-by: Felix Lobert <felix.lobert@thuenen.de>
    Co-authored-by: felixlobert <felixlobert@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Integrating SAR data into FORCE.',
-    author='Felix Lobert',
-    license='MIT',
+    version="0.1.0",
+    description="Integrating SAR data into FORCE.",
+    author="Felix Lobert",
+    license="MIT",
 )
